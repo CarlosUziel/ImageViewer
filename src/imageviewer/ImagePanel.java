fb6cf32f990298310b3b98e7568d10136760acaf
CarlosUziel <cauzpema@gmail.com>
2015-01-06 23:37:06 +0000

    Working (Not as intended yet)

--- src/imageviewer/ImagePanel.java
+++ src/imageviewer/ImagePanel.java
@@ -1,46 +1,73 @@
 package imageviewer;
 
 import java.awt.BorderLayout;
-import static java.awt.BorderLayout.CENTER;
-import static java.awt.BorderLayout.SOUTH;
-import java.awt.event.ActionListener;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import javax.swing.JPanel;
 
 class ImagePanel extends JPanel {
 
     private Image image;
     private ButtonPanel buttonPanel;
-    private PaintPanel paintPanel;
+    private BufferedImage bImage;
 
     public ImagePanel() {
         this.setLayout(new BorderLayout());
-        createPaintPanel();
-        createButtonPanel();
     }
 
     public void load(File directory) {
+        System.out.println(directory.getAbsolutePath());
         image = new GalleryLoader(directory.listFiles(new ImageFilter())).load();
-        if (paintPanel == null) {
-            System.out.println("paintpanel is null");
+        if (image != null) {
+            setImage(image);
         }
-        paintPanel.setImage(image);
     }
 
-    private void createPaintPanel() {
-        paintPanel = new PaintPanel();
-        this.add(paintPanel, CENTER);
+    public void setImage(Image image) {
+        this.image = image;
+        try {
+            bImage = ImageLoader.load(image.getPath());
+            repaint();
+        } catch (IOException ex) {
+        }
     }
 
-    private void createButtonPanel() {
-        buttonPanel = new ButtonPanel(createCommands());
-        this.add(buttonPanel, SOUTH);
+    public Image getImage() {
+        return image;
     }
 
-    private ActionListener[] createCommands() {
-        return new ActionListener[]{
-            new PrevImageCommand(paintPanel),
-            new NextImageCommand(paintPanel)
-        };
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        int width = getImageWidth();
+        int height = getImageHeight();
+        g.drawImage(bImage, (getWidth()-width)/2, (getHeight()-height)/2, width, height, null);
+    }
+
+    private int getImageWidth() {
+        if (bImage == null) {
+            return getWidth();
+        }
+        float aspectRatio = bImage.getHeight() / (float) bImage.getWidth();
+        if (getWidth() * aspectRatio < getHeight()) {
+            return getWidth();
+        } else {
+            return (int) (getHeight() / aspectRatio);
+        }
+    }
+
+    private int getImageHeight() {
+        if (bImage == null) {
+            return getWidth();
+        }
+        float aspectRatio = bImage.getHeight() / (float) bImage.getWidth();
+        if (getWidth() * aspectRatio < getHeight()) {
+            return (int)(getWidth()*aspectRatio);
+        } else {
+            return getHeight();
+        }
+
     }
 }
